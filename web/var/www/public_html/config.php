f990d92706cca23fb25049b90f3a81c7bcef2f6f
katWuzHere <kvc750@gmail.com>
2024-04-26 10:32:14 -0500

    manual clean commit head. Assume I've secured my passwords at this step. Unfortunately most developers stop here

--- web/var/www/public_html/config.php
+++ web/var/www/public_html/config.php
@@ -1,13 +1,1 @@
-<?php
-    $dbms = 'mysql';
-    # Informative
-    $dbhost = 'localhost';
-    $dbport = '';
-    # Informative
-    $dbname = 'main';
-    # Informative
-    $dbuser = 'root';
-    # Risk
-    $dbpasswd = 'pass123';	
-    $connection = mysql_connect($dbhost, $dbuser, $dbpasswd) or die('Connection failed!');
-?>
+clean
